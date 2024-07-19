daafcc9b5d1c21cd4e17d19814cffaa135674f8d
AcePeQ <maciej.nojszewski22@gmail.com>
2024-07-19 17:41:54 +0200

    feat: split code with react router lazy function

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-import Homepage from "./pages/Homepage/Homepage";
-import Contact from "./features/contact/Contact";
-import Layout from "./pages/Layout/Layout";
-import MobileCar from "./features/mobilecar/MobileCar";
-import Services from "./features/servicesPage/Services";
+import Loader from "./components/loader/Loader";
+
+const Homepage = lazy(() => import("./pages/Homepage/Homepage"));
+const Contact = lazy(() => import("./features/contact/Contact"));
+const Layout = lazy(() => import("./pages/Layout/Layout"));
+const MobileCar = lazy(() => import("./features/mobilecar/MobileCar"));
+const Services = lazy(() => import("./features/servicesPage/Services"));
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route element={<Layout />}>
-          <Route path="/services" element={<Services />} />
-          <Route path="/mobile-car" element={<MobileCar />} />
-          <Route path="/contact" element={<Contact />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route element={<Layout />}>
+            <Route path="/services" element={<Services />} />
+            <Route path="/mobile-car" element={<MobileCar />} />
+            <Route path="/contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
--- src/features/contact/components/map/Map.jsx
+++ src/features/contact/components/map/Map.jsx
@@ -5,8 +5,8 @@ export default function Map() {
     <div className={styles.map_container}>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2445.5526818940225!2d21.756282776921413!3d52.197003171978345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471f30c2b4edbb1b%3A0x684186c5a394fced!2sMobilna%20Wulkanizacja%20Serwis%20i%20sprzeda%C5%BC%20opon!5e0!3m2!1spl!2spl!4v1720976710909!5m2!1spl!2spl"
-        loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
+        loading="lazy"
         className={styles.map}
       ></iframe>
     </div>
