862545a6b3ec06a4d3722abbf0242edb92a0da45
AcePeQ <maciej.nojszewski22@gmail.com>
2024-07-22 20:59:02 +0200

    feat: add tire animation rotate

--- src/features/homepage/components/tireAnimation/Tire.jsx
+++ src/features/homepage/components/tireAnimation/Tire.jsx
@@ -1,8 +1,20 @@
+import { useRef } from "react";
 import styles from "./Tire.module.css";
 
+import { motion, useInView, useScroll, useSpring } from "framer-motion";
+
 function Tire() {
+  const img = useRef(null);
+  let { scrollY } = useScroll({
+    target: img,
+    offset: ["start end", "end end"],
+  });
+  const rotate = useSpring(scrollY);
+
   return (
-    <img
+    <motion.img
+      ref={img}
+      style={{ rotate: rotate, translateX: "-50%" }}
       className={styles.tire}
       src="/src/assets/3dtire.png"
       alt="3D Tire with rotate 360 animation while scrolling page"
