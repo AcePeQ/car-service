01a2efe5599a4538062f3f12910db59d9a60ced0
AcePeQ <maciej.nojszewski22@gmail.com>
2024-07-23 23:55:02 +0200

    feat: add whileInView animations for Contact page

--- src/features/contact/Contact.jsx
+++ src/features/contact/Contact.jsx
@@ -4,16 +4,25 @@ import Informations from "../../components/informations/Informations";
 import Map from "./components/map/Map";
 import Form from "./components/formEmail/Form";
 
+import { FadeIn } from "../../utils/Animations";
+import { motion } from "framer-motion";
+
 function Contact() {
   return (
     <div className={styles.layout}>
       <div className={styles.container}>
         <Form />
 
-        <div className={styles.informations}>
+        <motion.div
+          variants={FadeIn("down", 0.1, 50, 0.7)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true }}
+          className={styles.informations}
+        >
           <Informations type="contact_page" />
           <Map />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
